5decd33b7b64e96c63e67d7156fb51ff977642b0
nick5217 <13764558170@163.com>
2018-01-24 17:05:48 +0800

    估算模块80%

--- KDEG/kdege/src/components/home.vue
+++ KDEG/kdege/src/components/home.vue
@@ -1,18 +1,20 @@
-
 <!--首页-->
 <template>
   <body>
   <my-header></my-header>
-  <div style="; width:100%;height:100%;margin:0px;position: relative ">//全部的东西
+  <div style="; width:100%;height:100%;margin:0px;position: relative ">
     <img src="../assets/首页.png" width="100%" style="margin-top: 0px"/>//首页的图片 by刘千羽
     <el-button type="warning" style="position: absolute;left: 46.2%" onclick="window.open('http://admin.ksudi.com')">
       E键下单
-    </el-button>  //这个是下单的按钮 在中间，链接是快速递做的登陆页面
+    </el-button>
+    //这个是下单的按钮 在中间，链接是快速递做的登陆页面
     <el-button type="warning"
                style="outline: none;position: absolute;top:3790px;left: 7%;color: white;text-decoration: none">
       <router-link to="/interview" style="color: white;text-decoration: none">加入我们</router-link>
-    </el-button>//这个是放在二维码上的加入按钮，点击跳到招募页面
-    <video src="../assets/宣传视频.mp4" width="40%" controls="controls"></video>//宣传视频
+    </el-button>
+    //这个是放在二维码上的加入按钮，点击跳到招募页面
+    <video src="../assets/宣传视频.mp4" width="40%" controls="controls"></video>
+    //宣传视频
 
     <!--预估邮价的部分 在页面中间-->
     <div id="guess" style="vertical-align: center">
@@ -21,12 +23,18 @@
       &nbsp&nbsp
       &nbsp&nbsp
       寄件地址:&nbsp&nbsp
-      <el-input placeholder="请输入地址" style="width: 20%"></el-input>
-      &nbsp&nbsp 收件地址:&nbsp&nbsp;<el-input placeholder="请输入地址" style="width: 20%"></el-input>
+      <el-input placeholder="请输入地址" style="width: 12%" value="" id="start"></el-input>
+      &nbsp&nbsp
+      收件地址:&nbsp&nbsp;
+      <el-input placeholder="请输入地址" style="width: 12%" value="" id="end"></el-input>
+      &nbsp&nbsp
+      重量（公斤）:&nbsp&nbsp;
+      <el-input placeholder="请输入重量" style="width: 12%" value="" id="weighth"></el-input>
       <!--<br><br>-->
       <!--&nbsp&nbsp&nbsp&nbsp-->
       &nbsp&nbsp
       &nbsp&nbsp
+      <div id="allmap" style="display: none"></div>
       <el-dropdown>
         <el-button type="primary">
           选择产品<i class="el-icon-arrow-down el-icon--right"></i>
@@ -39,58 +47,83 @@
         </el-dropdown-menu>
       </el-dropdown>
       <br>
-      <center>
-        <button class="btn btn-success btn-lg"
-                style="outline: none;vertical-align: center"
-                onclick="s1()">预估邮费
-        </button>
-        <br>
-        <!--暂时的假数据-->
-        <div id="t1">
-          <el-alert
-            title="您的邮费为 15 元"
-            type="success"
-            center
-            show-icon
-          >
-          </el-alert>
 
-        </div>
 
-      </center>
+      <button class="btn btn-success btn-lg"
+              style="outline: none;position: absolute;left: 40.2%"
+              onclick="s2()">预估邮费
+      </button>
+      <br>
+      <!--暂时的假数据-->
+      <div id="t1">
+        <el-alert
+          :title="title1"
+          type="success"
+          center
+          show-icon
+        >
+        </el-alert>
+
+      </div>
+
+
     </div>
   </div>
   <script type="application/javascript">
     // import $ from 'jquery'
+    var map = new BMap.Map("allmap")
+    map.enableScrollWheelZoom();//启用滚轮放大缩小，默认禁用
+    map.enableContinuousZoom();
+    var myGeo = new BMap.Geocoder();
+    var stri;
+    var pointA, pointB;
+    var price;
 
-    function s1() {
-      var tx1 = document.getElementById("t1");
-      tx1.style.display = "block";
+    function s2() {
+      // var dis =map.getDistance(pointA, pointB)[0];
+      var s = document.getElementById("start");
+      var e = document.getElementById("end");
+      var w = document.getElementById("weight");
+
+      myGeo.getPoint(s.value, function (point) {
+        map.centerAndZoom(point, 16);
+        map.addOverlay(new BMap.Marker(point));
+        pointA = point;
+        myGeo.getPoint(e.value, function (point) {
+          map.centerAndZoom(point, 16);
+          map.addOverlay(new BMap.Marker(point));
+          pointB = point;
+          // alert("距离"+(map.getDistance(pointA, pointB)).toFixed(2))
+          var dis = (map.getDistance(pointA, pointB)).toFixed(2);
+
+
+          if (w > 3 && w <= 10) {
+            // price1= 3000 * 10 + (dis - 3000) * 2
+            price1 = (w - 3) * 2;
+            if (W > 10) {
+              price1 = 14 + (w - 10) * 5;
+            }
+
+
+          } else {
+            price1 = 0
+
+          }
+          if (dis > 3000) {
+            price2 = (dis - 3000) * 2;
+          } else {
+            price2 = 0
+          }
+          price = price1 + price2 + 10;
+          stri = "邮费为" + (price) + "元"
+          var tx1 = document.getElementById("t1");
+          tx1.style.display = "block";
+          alert(stri);
+        }, "上海市")
+      }, "上海市")
     }
   </script>
-  <script type="application/javascript">
-    import $ from 'jquery'
 
-    function open1() {
-      this.$alert('这是一段内容', '标题名称', {
-        confirmButtonText: '确定',
-        callback: action => {
-          this.$message({
-            type: 'info',
-            message: 'action: ${ action }'
-          });
-        }
-      });
-    }
-
-    $(document).ready(function () {
-      $("button").click(function () {
-        $("p").hide();
-      });
-    });
-
-
-  </script>
   <my-footer></my-footer>
   </body>
 
@@ -101,9 +134,11 @@
 
   export default {
     data() {
+      title1: stri
       return {
         activeIndex: '1',
         activeIndex2: '1',
+
         // img1: [{url: require('../assets/快递e哥启动仪式/何放-陈斌合影（20171101）.jpg')},
         //   {url: require('../assets/快递e哥启动仪式/快递E哥产品启动仪式01.jpg')},
         //   {url: require('../assets/快递e哥启动仪式/快递E哥产品启动仪式02.jpg')},
@@ -113,22 +148,9 @@
       }
       methods: {
 
-        // }
-        // open2()
-        // {
-        //   this.$message({
-        //     message: '恭喜你，这是一条成功消息',
-        //     type: 'success'
-        //   });
-        // }
-        $(document).ready(function () {
-          $("button").click(function () {
-            $("p").hide();
-          });
-        });
       }
-    }
 
+    }
 
   }
 
@@ -231,7 +253,7 @@
   #guess {
     position: absolute;
     top: 1296px;
-    left: 20%;
+    left: 10%;
     padding-top: 0px;
   }
 
